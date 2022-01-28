50363c3f94675ac021046c11461f9aa9781c251e
georgetim1998 <georgetim1998@gmail.com>
2022-01-28 21:06:03 +0300

    added questions but null constraint doesnt work

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_165353) do
+ActiveRecord::Schema.define(version: 2022_01_28_171949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body"
+    t.integer "test_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
