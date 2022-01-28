eaa50ec69952026b359dea299ac5aab93de9bf68
georgetim1998 <georgetim1998@gmail.com>
2022-01-28 22:42:29 +0300

    question test_id constraint

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_28_171949) do
+ActiveRecord::Schema.define(version: 2022_01_28_193944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "questions", force: :cascade do |t|
     t.string "body", null: false
-    t.integer "test_id"
+    t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
