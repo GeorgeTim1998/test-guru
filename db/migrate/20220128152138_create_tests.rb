8fcf180b5d57dcfc49ac35e75657505e20a0fd56
georgetim1998 <georgetim1998@gmail.com>
2022-01-31 00:57:59 +0300

    added references

--- db/migrate/20220128152138_create_tests.rb
+++ db/migrate/20220128152138_create_tests.rb
@@ -3,6 +3,8 @@ class CreateTests < ActiveRecord::Migration[6.0]
     create_table :tests do |t|
       t.string :title, null: false, from: nil, to: 1
       t.integer :level, null: false
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220128194412_create_answers.rb
+++ db/migrate/20220128194412_create_answers.rb
@@ -2,8 +2,8 @@ class CreateAnswers < ActiveRecord::Migration[6.0]
   def change
     create_table :answers do |t|
       t.string :body, null: false
-      t.integer :test_id, null: false
       t.string :correct, null: false, from: nil, to: '1'
+      t.references :question, null: false, foreign_key: true
 
       t.timestamps
     end
