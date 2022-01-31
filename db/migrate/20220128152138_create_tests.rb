ca6af72be4dce56af940e52e675419a35fe4f51b
georgetim1998 <georgetim1998@gmail.com>
2022-01-31 14:15:24 +0300

    default test name

--- db/migrate/20220128152138_create_tests.rb
+++ db/migrate/20220128152138_create_tests.rb
@@ -1,7 +1,7 @@
 class CreateTests < ActiveRecord::Migration[6.0]
   def change
     create_table :tests do |t|
-      t.string :title, null: false, from: nil, to: 1
+      t.string :title, null: false, from: nil, to: 'test name'
       t.integer :level, null: false
       t.references :category, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
