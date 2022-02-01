a48fac585e4a71afa2e9b9ad08f18fbc777a7fcc
georgetim1998 <georgetim1998@gmail.com>
2022-02-01 10:53:03 +0300

    renamed method

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,5 @@
 class Test < ApplicationRecord
-  def self.test_name_sort(category)
+  def self.test_name_desc_by_category(category)
     category_id = Category.where(category: category).pluck(:id)
     Test.where(category_id: category_id).order(title: :desc).pluck(:title)
   end
