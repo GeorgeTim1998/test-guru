f305dd57e023858c5e1d0b36aa335c6b73672d05
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 01:49:14 +0300

    join via linking table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   def tests_of_difficulty(level)
-    Test.where(author_id: id, level: level)
+    Test.joins('JOIN results ON test_id = tests.id').where('level = ? AND results.user_id = ?',
+                                                           level, id)
   end
 end
