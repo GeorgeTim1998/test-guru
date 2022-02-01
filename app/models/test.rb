43c0dcdd97fccce3a7b387b08b138f89ee88464f
georgetim1998 <georgetim1998@gmail.com>
2022-02-01 10:46:18 +0300

    class method (no valid)

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.test_name_sort(category)
+    category_id = Category.where(category: category).pluck(:id)
+    Test.where(category_id: category_id).order(title: :desc).pluck(:title)
+  end
 end
