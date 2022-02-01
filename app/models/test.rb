546eb8092763258dd85edc1e849f2ccaf5053bb5
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 00:19:18 +0300

    Test via join

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,6 @@
 class Test < ApplicationRecord
   def self.test_name_desc_by_category(category)
-    category_id = Category.where(category: category).pluck(:id)
-    Test.where(category_id: category_id).order(title: :desc).pluck(:title)
+    Test.joins('JOIN categories ON categories.id = category_id').where('categories.category = ?',
+                                                                       category).order(title: :desc).pluck(:title)
   end
 end
