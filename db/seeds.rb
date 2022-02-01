d472d20f0bec0c8825bf2146b1a923bb7ced136e
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 01:23:11 +0300

    seed update w/ results

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,17 +6,24 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Result.destroy_all
+Answer.destroy_all
+Question.destroy_all
+Test.destroy_all
+User.destroy_all
+Category.destroy_all
+
 categories = Category.create!([{ category: 'Programming skills' }, { category: 'Speaking skills' },
                                { category: 'Math skills' }])
 users = User.create!([{ name: 'George' }, { name: 'Masha' }, { name: 'Vika' }])
 
-test = Test.create!(title: 'Ruby', level: 2, category_id: categories[0].id, author_id: users[0].id)
-Test.create!(title: 'HTML', level: 3, category_id: categories[0].id, author_id: users[0].id)
-Test.create!(title: 'Rails', level: 1, category_id: categories[0].id, author_id: users[0].id)
+tests = Test.create!([{ title: 'Ruby', level: 2, category_id: categories[0].id, author_id: users[0].id },
+                      { title: 'HTML', level: 3, category_id: categories[0].id, author_id: users[0].id },
+                      { title: 'Rails', level: 1, category_id: categories[0].id, author_id: users[0].id }])
 
-question = Question.create!(body: 'What is a class?', test_id: test.id)
-Question.create!(body: 'what is an object?', test_id: test.id)
-Question.create!(body: 'What is an instance method?', test_id: test.id)
+question = Question.create!(body: 'What is a class?', test_id: tests[0].id)
+Question.create!(body: 'what is an object?', test_id: tests[0].id)
+Question.create!(body: 'What is an instance method?', test_id: tests[0].id)
 
 Answer.create!(body: 'A class is written by a programmer in a defined structure to create an object.',
                correct: true, question_id: question.id)
@@ -24,3 +31,7 @@ Answer.create!(body: 'Object is a variable, a data structure, a function, or a m
                question_id: question.id)
 Answer.create!(body: 'Methods which require an object of its class to be created before it can be called.',
                correct: true, question_id: question.id)
+
+Result.create!(user_id: users[0].id, test_id: tests[0].id)
+Result.create!(user_id: users[0].id, test_id: tests[1].id)
+Result.create!(user_id: users[0].id, test_id: tests[2].id)
