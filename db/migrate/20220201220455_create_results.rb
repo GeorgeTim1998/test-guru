bc9f0474d7f56cbf69d566daad7e33a863210a61
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 01:07:59 +0300

    rubocop

--- db/migrate/20220201220455_create_results.rb
+++ db/migrate/20220201220455_create_results.rb
@@ -3,7 +3,7 @@ class CreateResults < ActiveRecord::Migration[6.0]
     create_table :results do |t|
       t.references :user, null: false, foreign_key: true
       t.references :test, null: false, foreign_key: true
-      
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,64 +10,63 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_220455) do
-
+ActiveRecord::Schema.define(version: 20_220_201_220_455) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "answers", force: :cascade do |t|
-    t.string "body", null: false
-    t.boolean "correct", null: false
-    t.bigint "question_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["question_id"], name: "index_answers_on_question_id"
+  create_table 'answers', force: :cascade do |t|
+    t.string 'body', null: false
+    t.boolean 'correct', null: false
+    t.bigint 'question_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['question_id'], name: 'index_answers_on_question_id'
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string "category", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'categories', force: :cascade do |t|
+    t.string 'category', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "questions", force: :cascade do |t|
-    t.string "body", null: false
-    t.bigint "test_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_questions_on_test_id"
+  create_table 'questions', force: :cascade do |t|
+    t.string 'body', null: false
+    t.bigint 'test_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['test_id'], name: 'index_questions_on_test_id'
   end
 
-  create_table "results", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "test_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["test_id"], name: "index_results_on_test_id"
-    t.index ["user_id"], name: "index_results_on_user_id"
+  create_table 'results', force: :cascade do |t|
+    t.bigint 'user_id', null: false
+    t.bigint 'test_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['test_id'], name: 'index_results_on_test_id'
+    t.index ['user_id'], name: 'index_results_on_user_id'
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.integer "level", null: false
-    t.bigint "category_id", null: false
-    t.bigint "author_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_tests_on_author_id"
-    t.index ["category_id"], name: "index_tests_on_category_id"
+  create_table 'tests', force: :cascade do |t|
+    t.string 'title', null: false
+    t.integer 'level', null: false
+    t.bigint 'category_id', null: false
+    t.bigint 'author_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['author_id'], name: 'index_tests_on_author_id'
+    t.index ['category_id'], name: 'index_tests_on_category_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'users', force: :cascade do |t|
+    t.string 'name', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "answers", "questions"
-  add_foreign_key "questions", "tests"
-  add_foreign_key "results", "tests"
-  add_foreign_key "results", "users"
-  add_foreign_key "tests", "categories"
-  add_foreign_key "tests", "users", column: "author_id"
+  add_foreign_key 'answers', 'questions'
+  add_foreign_key 'questions', 'tests'
+  add_foreign_key 'results', 'tests'
+  add_foreign_key 'results', 'users'
+  add_foreign_key 'tests', 'categories'
+  add_foreign_key 'tests', 'users', column: 'author_id'
 end
