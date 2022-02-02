f8bd97587b39599b01b23fbf7227aeba2ec6d89f
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 12:46:17 +0300

    q

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,6 +2,7 @@ class Test < ApplicationRecord
   def self.test_name_desc_by_category(category_name)
     joins('JOIN categories ON categories.id = category_id')
       .where('categories.category = ?', category_name)
-      .order(title: :desc).pluck(:title)
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
