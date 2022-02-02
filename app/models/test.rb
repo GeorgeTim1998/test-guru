3f2bc3a98216dc5963e883ca40ee7ded7885a6b7
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 11:33:00 +0300

    join fix. split line fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,7 @@
 class Test < ApplicationRecord
-  def self.test_name_desc_by_category(category)
-    Test.joins('JOIN categories ON categories.id = category_id').where('categories.category = ?',
-                                                                       category).order(title: :desc).pluck(:title)
+  def self.test_name_desc_by_category(category_name)
+    joins('JOIN categories ON categories.id = category_id')
+      .where('categories.category = ?', category_name)
+      .order(title: :desc).pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def tests_of_difficulty(level)
-    Test.joins('JOIN results ON test_id = tests.id').where('level = ? AND results.user_id = ?',
-                                                           level, id)
+    Test.joins('JOIN results ON test_id = tests.id')
+        .where('level = ? AND results.user_id = ?', level, id)
   end
 end
