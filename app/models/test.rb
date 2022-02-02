4135316729c68da53ead56a76a5f5b7306133929
georgetim1998 <georgetim1998@gmail.com>
2022-02-02 15:20:23 +0300

    hash in where

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,7 @@
 class Test < ApplicationRecord
   def self.test_name_desc_by_category(category_name)
     joins('JOIN categories ON categories.id = category_id')
-      .where('categories.category = ?', category_name)
+      .where(categories: { category: category_name })
       .order(title: :desc)
       .pluck(:title)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   def tests_of_difficulty(level)
     Test.joins('JOIN results ON test_id = tests.id')
-        .where('level = ? AND results.user_id = ?', level, id)
+        .where(level: level, results: { user_id: id })
   end
 end
