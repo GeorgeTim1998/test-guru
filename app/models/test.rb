7b8890eb1c71265462d3a37ebf51752ae87fa829
georgetim1998 <georgetim1998@gmail.com>
2022-02-03 00:11:33 +0300

    modified joins in test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,10 +1,13 @@
 class Test < ApplicationRecord
   belongs_to :category
+  # belongs_to :author, class_name: 'User', inverse_of: 'author_tests'
+
   has_many :questions
   has_many :results
   has_many :users, through: :results
+
   def self.test_name_desc_by_category(name)
-    joins('JOIN categories ON categories.id = category_id')
+    joins(:category)
       .where(categories: { name: name })
       .order(title: :desc)
       .pluck(:title)
