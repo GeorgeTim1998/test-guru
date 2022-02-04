e9db623f7f35a74de385e89f6f3b4c1b2824d42d
georgetim1998 <georgetim1998@gmail.com>
2022-02-04 17:35:04 +0300

    scopes and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   has_many :tests, through: :results
   has_many :created_tests, class_name: 'Test', inverse_of: 'author', foreign_key: 'author_id'
 
-  def tests_of_difficulty(level)
-    tests.where(level: level)
+  validates :email, presence: true
+
+  def tests_by_difficulty(level)
+    Test.results_by_level(self, level)
   end
 end
