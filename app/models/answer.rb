8efe0bfb9ef682a16e3fb784dde4b5154fd9e194
georgetim1998 <georgetim1998@gmail.com>
2022-02-04 17:35:04 +0300

    scopes and validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,15 @@
 class Answer < ApplicationRecord
   belongs_to :question
+
+  validates :body, presence: true
+
+  validate :validate_answers_count
+
+  scope :correct, -> { where(correct: true) }
+
+  private
+
+  def validate_answers_count
+    errors.add(:base, 'Amount of answers must be 1..4') if question.answers.count >= 4
+  end
 end
