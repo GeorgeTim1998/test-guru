6246115cddd39fe598b88da040a4b67e4ccbb236
georgetim1998 <georgetim1998@gmail.com>
2022-02-04 14:52:33 +0300

    Revert "Test scope"
    This reverts commit d6e44549ae84e165ade8c7544e208919d847f9e7.

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,10 +6,6 @@ class Test < ApplicationRecord
   has_many :results, dependent: :destroy
   has_many :users, through: :results
 
-  scope :easy_level_tests, -> { where(level: 0..1) }
-  scope :medium_level_tests, -> { where(level: 2..4) }
-  scope :hard_level_tests, -> { where(level: 5..Float::INFINITY) }
-
   def self.test_name_desc_by_category(name)
     joins(:category)
       .where(categories: { name: name })
