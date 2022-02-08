5e62762f3f3c58be61cac832fa6074d347684989
georgetim1998 <georgetim1998@gmail.com>
2022-02-08 13:29:21 +0300

    only create question left

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,32 @@
+class QuestionsController < ApplicationController
+  before_action :find_test, only: %(index)
+  before_action :find_question, only: %(show destroy)
+
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
+  def index
+    render inline: '<% @test.questions.each do |question| %><p><%= question.body %></p><% end %>'
+  end
+
+  def show
+    render inline: '<%= @question.body %>'
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Question not found!'
+  end
+end
