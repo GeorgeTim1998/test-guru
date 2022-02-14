c4b62b8ace6a1d0a80fecf55730eea96f266bd86
georgetim1998 <georgetim1998@gmail.com>
2022-02-14 16:03:32 +0300

    answer views

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -38,6 +38,10 @@ class AnswersController < ApplicationController
     @question = Question.find(params[:question_id])
   end
 
+  def set_answer
+    @answer = Answer.find(params[:id])
+  end
+
   def answer_params
     params.require(:answer).permit(:body, :correct)
   end
