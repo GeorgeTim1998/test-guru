577e71bfe64cbe13c86539905ada4d5d4a2e11c8
georgetim1998 <georgetim1998@gmail.com>
2022-02-15 00:12:54 +0300

    fixed association bug

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ApplicationRecord
   belongs_to :test
 
   has_many :answers, dependent: :destroy
-  has_many :results, dependent: :nullify
+  has_many :results, dependent: :nullify, inverse_of: 'current_question', foreign_key: 'current_question_id'
 
   validates :body, presence: true
 end
