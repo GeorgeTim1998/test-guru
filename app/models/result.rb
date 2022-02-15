e8672c19a9e7e7bc5c853b16b4b83db2d7ccf5b5
georgetim1998 <georgetim1998@gmail.com>
2022-02-15 15:00:29 +0300

    merged validations

--- app/models/result.rb
+++ app/models/result.rb
@@ -5,8 +5,7 @@ class Result < ApplicationRecord
   belongs_to :user
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
-  before_validation :before_validation_set_next_question, on: :update
+  before_validation :before_validation_set_current_question, on: %i[create update]
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
@@ -27,18 +26,17 @@ class Result < ApplicationRecord
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
-    self.current_question_number = 1
-  end
-
-  def before_validation_set_next_question
+  def before_validation_set_current_question
     self.current_question = next_question
     self.current_question_number += 1
   end
 
   def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
+    if self.current_question_number.zero?
+      test.questions.first if test.present?
+    else
+      test.questions.order(:id).where('id > ?', current_question.id).first
+    end
   end
 
   def correct_answer?(answer_ids)
