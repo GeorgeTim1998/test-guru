a6cfd099ddceedec49dae2ee1876f0aa1c5e9e03
georgetim1998 <georgetim1998@gmail.com>
2022-02-15 12:38:23 +0300

    del callback

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,5 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show start]
-  before_action :find_user, only: :start
 
   def index
     @tests = Test.all
@@ -11,6 +10,7 @@ class TestsController < ApplicationController
   end
 
   def start
+    @user = User.first
     @user.tests.push(@test)
     redirect_to @user.result(@test)
   end
@@ -19,9 +19,5 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = User.first
   end
 end
