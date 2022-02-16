140e81f8358cc0c755b8207195975cc2fc1abad3
georgetim1998 <georgetim1998@gmail.com>
2022-02-16 15:16:43 +0300

    user auth fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
     if @user.save!
-      redirect_to test_path
+      redirect_to tests_path
     else
       render :new
     end
@@ -16,6 +15,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
