17f82aeb06405452d3bf833a9f26c833c8fbb45a
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 00:20:09 +0300

    admin tests

---
+++ app/controllers/admin/tests_controller.rb
@@ -0,0 +1,22 @@
+class Admin::TestsController < Admin::BaseController
+  before_action :find_test, only: %i[show start]
+
+  def index
+    @tests = Test.all
+  end
+
+  def show
+    @questions = @test.questions
+  end
+
+  def start
+    current_user.tests.push(@test)
+    redirect_to current_user.result(@test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:id])
+  end
+end
