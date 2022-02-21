80e510fbdc3e1a6f1adb115478d075c225e6f92b
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 00:12:05 +0300

    routes updated (deleted obsolete resources)

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tests#index'
 
-  get :signup, to: 'users#new'
-  get :login, to: 'sessions#new'
-  delete :logout, to: 'sessions#destroy'
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sigh_out: :logout }
 
-  resources :users, only: :create
-  resources :sessions, only: :create
-
-  resources :tests do
-    resources :questions, shallow: true, except: :index do
-      resources :answers, shallow: true, except: :index
-    end
-
+  resources :tests, only: :index do
     member do
       post :start
     end
@@ -22,6 +13,14 @@ Rails.application.routes.draw do
   resources :results, only: %i[show update] do
     member do
       get :summary
+    end
+  end
+
+  namespace :admin do
+    resources :tests do
+      resources :questions, shallow: true, except: :index do
+        resources :answers, shallow: true, except: :index
+      end
     end
   end
 end
