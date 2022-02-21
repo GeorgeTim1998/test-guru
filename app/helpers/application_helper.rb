877c9aae1c9f0fb96ea320b8243f9a7b3c301539
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 02:23:22 +0300

    doubles of devise navigation del

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,14 +10,4 @@ module ApplicationHelper
   def flash_message(method)
     tag.p(flash[method], class: 'method') if flash[method]
   end
-
-  def user_actions
-    if user_signed_in?
-      tag.p("Welcome, #{current_user.email} Guru") +
-        link_to(' Logout', destroy_user_session_path, method: :delete, data: { confirm: 'U sure?' })
-    else
-      link_to('Login', new_user_session_path) + tag.p(' ') +
-        link_to('Signup', new_user_registration_path)
-    end
-  end
 end
