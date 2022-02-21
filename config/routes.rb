0bf3c2de95dc5d14edff4da2f961867c3f04a7c2
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 00:57:39 +0300

    sign_up: :signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sigh_out: :logout }
+  devise_for :users, path: :gurus, path_names: { sign_in: :login, sigh_out: :logout, sign_up: :signup }
 
   resources :tests, only: :index do
     member do
