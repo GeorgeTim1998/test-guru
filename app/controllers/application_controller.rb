3ad5a49076633149c6be709fd4e314c5d0adbd3e
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 02:44:48 +0300

    override devise helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.instance_of?(Admin)
+      admin_tests_path
+    else
+      root_path
+    end
+  end
 end
