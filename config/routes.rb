e19cdb664f67e37118c2d61d83a37c2712dfa512
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 02:09:19 +0300

    sign up name fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'tests#index'
 
-  devise_for :users, path: :gurus, path_names: { sign_in: :login, sigh_out: :logout, sign_up: :signup }
+  devise_for :users, path: :gurus,
+                     controllers: { registrations: 'users/registrations' },
+                     path_names: { sign_in: :login, sigh_out: :logout, sign_up: :signup }
 
   resources :tests, only: :index do
     member do
