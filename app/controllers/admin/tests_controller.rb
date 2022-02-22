50dceb07a005d7859a9fa8ae30b1e13a2b0980fd
georgetim1998 <georgetim1998@gmail.com>
2022-02-22 12:16:42 +0300

    tests controller expansion

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,5 +1,5 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :find_test, only: %i[show start]
+  before_action :find_test, only: %i[show destroy edit update]
 
   def index
     @tests = Test.all
@@ -9,9 +9,42 @@ class Admin::TestsController < Admin::BaseController
     @questions = @test.questions
   end
 
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = current_user.tests.build(test_params)
+
+    if @test.save
+      redirect_to admin_test_path(@test)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @test.update(test_params)
+      redirect_to admin_test_path(@test)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to admin_tests_path
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
   end
 end
