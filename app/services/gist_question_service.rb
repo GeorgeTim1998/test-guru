0eb392a9aa7bb91c36792051b6a43bf389c9ad1a
georgetim1998 <georgetim1998@gmail.com>
2022-02-23 22:37:18 +0300

    added internalization to service

---
+++ app/services/gist_question_service.rb
@@ -0,0 +1,34 @@
+class GistQuestionService
+  def initialize(question, client: nil)
+    @question = question
+    @test = @question.test
+    @client = client || Octokit::Client.new(access_token: github_access_token)
+  end
+
+  def call
+    @client.create_gist(gist_params)
+  end
+
+  private
+
+  def gist_params
+    {
+      description: I18n.t('description', title: @test.title),
+      files: {
+        I18n.t('file') => {
+          content: gist_content
+        }
+      }
+    }
+  end
+
+  def gist_content
+    content = [@question.body]
+    content += @question.answers.pluck(:body)
+    content.join("\n")
+  end
+
+  def github_access_token
+    Rails.application.credentials.github_access_token
+  end
+end
