25ddc36ee0eebaa561604185f9f8727c2cd05404
georgetim1998 <georgetim1998@gmail.com>
2022-02-23 23:58:14 +0300

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -13,12 +13,14 @@ Rails.application.routes.draw do
   end
 
   resources :results, only: %i[show update] do
+    resources :gists, only: :create
     member do
       get :summary
     end
   end
 
   namespace :admin do
+    resources :gist, only: :index
     resources :tests do
       resources :questions, shallow: true, except: :index do
         resources :answers, shallow: true, except: :index
