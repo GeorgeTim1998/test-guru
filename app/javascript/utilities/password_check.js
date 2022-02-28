a10021a62dff9181b66363e8e92337f95b12a144
georgetim1998 <georgetim1998@gmail.com>
2022-02-28 22:50:04 +0300

    fixed addEvent

--- app/javascript/utilities/password_check.js
+++ app/javascript/utilities/password_check.js
@@ -1,9 +1,9 @@
-
-document.addEventListener("input", function() {
+window.onload = function() {
   let forms = document.forms[0]
-  
-  if (forms.user_password_confirmation) { comparePasswordInputs(forms) }
-})
+  if (forms.user_password_confirmation) {
+    document.addEventListener("input", function() { comparePasswordInputs(forms) }) 
+  }
+}
 
 function comparePasswordInputs(forms) {
   
@@ -13,14 +13,11 @@ function comparePasswordInputs(forms) {
   if (confirmation_value === '') {
     forms.querySelectorAll(".octicon-check-circle").forEach((octicon) => octicon.classList.add("hide"))
     forms.querySelectorAll(".octicon-circle-slash").forEach((octicon) => octicon.classList.add("hide"))
-    console.log('Not yet')
   } else if (value === confirmation_value) {
     forms.querySelectorAll(".octicon-check-circle").forEach((octicon) => octicon.classList.remove("hide"))
     forms.querySelectorAll(".octicon-circle-slash").forEach((octicon) => octicon.classList.add("hide"))
-    console.log('Bingo!')
   } else {
     forms.querySelectorAll(".octicon-circle-slash").forEach((octicon) => octicon.classList.remove("hide"))
     forms.querySelectorAll(".octicon-check-circle").forEach((octicon) => octicon.classList.add("hide"))
-    console.log('Wrong!')
   }
 }
