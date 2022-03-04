eec76d057844fdfbf97cabf4207f02d55b8c6276
georgetim1998 <georgetim1998@gmail.com>
2022-03-04 17:48:40 +0300

    added validations to badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,9 @@
 class Badge < ApplicationRecord
   has_many :issued_badges, dependent: :destroy
   has_many :users, through: :issued_badges
+
+  validates :title, presence: true, uniqueness: true
+  validates :description, presence: true
+  validates :rule, presence: true
+  validates :image_url, presence: true
 end
